a56b7dea91f77bf44f83fb7d862b818aa9728ce0
raesii <leisolo@gmail.com>
2022-04-26 20:48:24 -0700

    added user module

--- config/connection.js
+++ config/connection.js
@@ -1,6 +1,8 @@
 const sequelize = require('sequelize');
 
-const sequelize = new sequelize('forumfeed_db', 'username', 'password', {
+require('dotenv').config();
+
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
     host: 'localhost',
     dialect: 'mysql',
     port: 3306
